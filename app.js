ff0e86fddd5aa9a7c8cac12de697bb00f16bf365
Justin Carlson <jcarlson1985420@gmail.com>
2016-04-01 21:45:00 -0500

    Add "Authorization" as a valid header

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(passport.initialize());
 // Enable CORS in the response headers
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
